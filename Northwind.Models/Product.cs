96141fd519ae2b117d8472cbbaa392c7eedc51fb
Alejandro Castiblanco Soler <David.Castiblanco@iq-online.com>
2019-01-23 14:28:00 -0500

    Se realiza checkin de los modelos creados en base a tabla de Base de Datos

---
+++ Northwind.Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Northwind.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public int Supplierld { get; set; }
+        public decimal UnitPrice { get; set; }
+        public string Package { get; set; }
+        public bool IsDiscontinued { get; set; }
+    }
+}
