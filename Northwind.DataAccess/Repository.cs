b31a9dea59a29221ecd81a4885f2eb78d965fa1d
alejandro <castiblancosolerd@gmail.com>
2019-01-23 23:13:05 -0500

    Checkin de implementacion Interfaz de metodos de Crud

---
+++ Northwind.DataAccess/Repository.cs
@@ -0,0 +1,35 @@
+﻿using Northwind.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Northwind.DataAccess
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        public bool Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int insert(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
