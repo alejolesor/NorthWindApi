5ea670892265977bbb1deb83ecec6a2e254b6499
Alejandro Castiblanco Soler <David.Castiblanco@iq-online.com>
2019-01-29 14:27:30 -0500

    Controler Customer con todos los metodos

--- NorthWin.WebAPI/Controllers/CustomerController.cs
+++ NorthWin.WebAPI/Controllers/CustomerController.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using Northwind.Models;
 using Northwind.UnitOfWork;
 
 namespace NorthWin.WebAPI.Controllers
@@ -13,8 +10,46 @@ namespace NorthWin.WebAPI.Controllers
         private readonly IUnitOfWork _unitOfWork;
         public CustomerController(IUnitOfWork unitOfWork)
         {
-            
+            _unitOfWork = unitOfWork;
         }
 
+        [HttpGet]
+        [Route("{id:int}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok(_unitOfWork.Customer.GetById(id));
+        }
+        [HttpGet]
+        [Route("GetPaginatedCustomer/{page:int}/{rows:int}")]
+        public IActionResult GetPaginatedCustomer(int page, int rows)
+        {
+            return Ok(_unitOfWork.Customer.CustomerPagedList(page, rows));
+        }
+        [HttpPost]
+        public IActionResult Post([FromBody]Customer customer)
+        {
+            if (ModelState.IsValid) return BadRequest();
+            return Ok(_unitOfWork.Customer.insert(customer));
+
+        }
+        [HttpPut]
+        public IActionResult Put([FromBody]Customer customer)
+        {
+            if (ModelState.IsValid && _unitOfWork.Customer.Update(customer))
+            {
+                return Ok(new { Message = "The Customer is Update" });
+            }
+            return BadRequest();
+
+        }
+        [HttpDelete]
+        public IActionResult Delete([FromBody]Customer customer)
+        {
+            if (customer.Id > 0)
+            {
+                return Ok(_unitOfWork.Customer.Delete(customer));
+            }
+            return BadRequest();
+        }
     }
 }
