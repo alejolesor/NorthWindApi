4549904a9ec3ff7772fb73541a34abe07012721d
Alejandro Castiblanco Soler <David.Castiblanco@iq-online.com>
2019-01-31 13:14:40 -0500

    Autenticacion JWT

---
+++ NorthWin.WebAPI/Authentication/JsonWebToken.cs
@@ -0,0 +1,10 @@
+﻿namespace NorthWin.WebAPI.Authentication
+{
+    public class JsonWebToken
+    {
+        public string Acces_Token { get; set; }
+        public string Token_Type { get; set; } = "bearer";
+        public int Expires_in { get; set; }
+        public string Refresh_Token { get; set; }
+    }
+}
