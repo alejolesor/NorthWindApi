dff0c89c514a35dab35d692cd3b09d45c6160460
Alejandro Castiblanco Soler <David.Castiblanco@iq-online.com>
2019-02-20 15:43:00 -0500

    CheckIn autenticacion

--- NorthWin.WebAPI/Controllers/TokenControllerController.cs
+++ NorthWin.WebAPI/Controllers/TokenControllerController.cs
@@ -2,9 +2,11 @@
 using NorthWin.WebAPI.Authentication;
 using Northwind.Models;
 using Northwind.UnitOfWork;
+using System;
 
 namespace NorthWin.WebAPI.Controllers
 {
+    [Route("api/Token")]
     public class TokenControllerController : Controller
     {
         private ITokenProvider _tokenProvider;
@@ -15,7 +17,20 @@ namespace NorthWin.WebAPI.Controllers
             _tokenProvider = tokenProvider;
             _unitOfwork = unitOfWork;
         }
-
-        public JsonWebToken Post(User userLogin)
+        [HttpPost]
+        public JsonWebToken Post([FromBody]User userLogin)
+        {
+            var user = _unitOfwork.User.ValidateUser(userLogin.Email, userLogin.Password);
+            if (user == null)
+            {
+                throw new UnauthorizedAccessException();
+            }
+            var token = new JsonWebToken
+            {
+                Acces_Token = _tokenProvider.CreateToken(user, DateTime.UtcNow.AddHours(8)),
+                Expires_in = 480//Minutes
+            };
+            return token;
+        }
     }
 }
